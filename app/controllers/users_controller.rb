7b38fb71cf0b51d5ffc2382cca19b5bfa846599f
Omar <ovargas@ovargas-desktop.(none)>
2009-12-12 17:09:21 -0600

    implement user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.find(:all)
+  end
+
   def new
     @user = User.new
   end
@@ -26,4 +31,10 @@ class UsersController < ApplicationController
       render :action => 'edit'
     end
   end
+
+  def show
+    @user = User.find(params[:id])    
+  end
+
+
 end
