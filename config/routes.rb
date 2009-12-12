be40d7a200ccd832ccea444d490884f5e6475d5f
Omar <ovargas@ovargas-desktop.(none)>
2009-12-12 16:17:20 -0600

    Create users and add the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.login "login", :controller =>"user_sessions", :action => "new"
   map.logout "logout", :controller =>"user_sessions", :action => "destroy"
 
   map.resources :user_sessions
   map.resources :users
-  map.root :users
+  map.root :controller => "dashboards"
+  map.resources :dashboards
 
 #   map.connect ':controller/:action/:id'
 #   map.connect ':controller/:action/:id.:format'
