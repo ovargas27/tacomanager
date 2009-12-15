768c3705dba016d8043ce68455d16c4de61ee605
Omar <ovargas@ovargas-desktop.(none)>
2009-12-15 04:35:46 -0600

    Dsiplay the pending orders in the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class DashboardsController < ApplicationController
   def index
+    today_start = Time.zone.now.strftime("%Y-%m-%d") + " 00:00:00" 
+    today_end = Time.zone.now.strftime("%Y-%m-%d") + " 23:59:59" 
+    @orders = Order.find(:all, :conditions => ["(created_at >= '#{today_start}' AND created_at <= '#{today_end}' )"])
   end
 end
