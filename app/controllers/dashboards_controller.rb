089d1918d3cbf949c6b5e9c47437f0d070c27251
Omar <ovargas@ovargas-desktop.(none)>
2009-12-21 18:34:44 -0600

    Add new mailers

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,4 +5,39 @@ class DashboardsController < ApplicationController
     @orders = Order.find(:all, :conditions => ["(created_at >= '#{today_start}' AND created_at <= '#{today_end}' )"])
     @total = @orders.collect{|order| order.total}.sum
   end
+  
+  def start
+    @phone_person, @money_person = get_random_users
+    @phone_person.
+    UserMailer.deliver_phone_person_advise(@phone_person)    
+    UserMailer.deliver_money_person_advise(@money_person)
+  end
+
+  def stop
+    # Remove privileges to phone person
+    # select the money person
+    # Remove privileges to money person
+    # send email with orders to phone person
+    # send email with orders to money person
+  end
+
+  private
+
+  def get_random_users()
+    phone
+    money
+    begin
+      users = User.find(:all)
+      x = users.size
+      phone = users.delete_at(rand(size))
+      raise Exception.new() if phone.nil?
+      money = users.delete_at(rand(size))
+      raise Exception.new() if money.nil?
+    rescue => e
+      flash[:notice] = "Error when select the users"
+      redirect_to dashboards_url
+    end
+    return phone, money
+  end
+
 end
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,8 +1,31 @@
 class UserMailer < ActionMailer::Base
   def registration_confirmation(user)
     recipients  user.email
-    from        "ovargas27@gmail.com"
+    from        "tacomanager@gmail.com"
     subject     "Thank you for Registering"
     body        :user => user
   end  
+  
+  def phone_person_advise(user)
+    recipients  user.email
+    from        "tacomanager@gmail.com"
+    subject     "Congrats today you are the phone person"
+    body        :user => user
+  end
+
+  def money_person_advise(user)
+    recipients  user.email
+    from        "tacomanager@gmail.com"
+    subject     "Congrats today you are the money person"
+    body        :user => user
+  end
+
+  def daily_orders_report(user, orders)
+    recipients  user.email
+    from        "tacomanager@gmail.com"
+    subject     "the orders for today"
+    body        :user => user, :orders => orders
+  end
+
+  
 end
