a83a9672d96663df6d022302499aadfb3f05e15b
Omar <ovargas27@gmail.com>
2011-04-12 20:05:26 -0500

    starting again

---
+++ README.md
@@ -0,0 +1,16 @@
+Tacomanager
+=============================
+
+####Español
+
+## Descripcion.
+El tacomanager es una applicacion web para organizar los pedidos de
+comida en oficinas, empresas o grupos de personas en general.
+
+
+#### English
+
+## Description
+The tacomanager is an web app to organize orders food in offices,
+companies or people groups
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tacomanager::Application.config.secret_token = 'f7dbf2b851c78dc28cca7ee2e9cafbcbb790406eeeba800b57b8718f6a834d1d129c1cc6f8a04ab9cb18d07d9799bc42839b621d7fe0acde78c846d0ff952ae4'
